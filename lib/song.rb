691081de1419153955f3d0a2831efa85b2c68ad5
Andy Adams <andrewdcadams@gmail.com>
2020-07-28 22:39:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,17 @@ class Song
   end
    
   def create_from_filename(fileString)
-    
+     new_artist = fileString.split("-")[0].to_s.strip
+    new_name = fileString.split("-")[1].to_s.chomp(".mp3").strip
+    #binding.pry
+    # newsong = create_by_name(new_name)
+    # newsong.artist_name = new_artist
+    new_song = self.new
+    new_song.name = new_name
+    new_song.artist_name = new_artist
+    @@all << new_song
+    new_song
+  end
   
   def self.all
     @@all
