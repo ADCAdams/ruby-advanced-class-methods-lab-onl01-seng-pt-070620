e1e21287ea88c44dcb32ff69a8e658a8ca73d630
Andy Adams <andrewdcadams@gmail.com>
2020-07-28 21:25:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,16 @@ class Song
     @@all << song
     song
   end
+  
   def self.new_by_name(newName)
     song = self.new 
     song.name = newName
     @@all << song 
     song
   end
+  
+  def self.create_by_name()
+  
   def self.all
     @@all
   end
