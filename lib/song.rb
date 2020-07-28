92edc4860c696af98da1ab6fd1e7c48cb4783888
Andy Adams <andrewdcadams@gmail.com>
2020-07-28 21:48:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,17 +28,8 @@ class Song
     nil
   end
   
-  def self.find_or_create_by_name(newString)
-    name_string = newString.to_s
-    # if find_by_name(name_string) == false 
-    #   puts "hllo"
-    #   create_by_name(name_string)
-    # else
-    #   puts "hi"
-    #   find_by_name(name_string)
-    # end
-    # item = find_by_name(name_string)
-    
+  def self.find_or_create_by_name(name_string)
+
      find_by_name(name_string) || create_by_name(name_string)
     
   end
